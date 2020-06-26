ee9b50ff4a0d451d6dcb5b7cbec19b1883a02600
Chanya Kukulprasong <chanjeedtu75@gmail.com>
2020-06-27 06:49:55 +0900

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const dotenv = require('dotenv').config();
+module.exports = {
+    entry: {
+        index: './src/pages/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|jpg|gif|svg|otf|eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(s[ac]ss|css)$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['@babel/preset-env', {
+                            targets: {
+                                esmodules: true,
+                            }
+                        }]],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": JSON.stringify(dotenv.parsed)
+        }),
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: 'index.html',
+            template: `${__dirname}/src/pages/index.html`,
+            favicon: `${__dirname}/src/assets/favicon.ico`,
+            inject: 'body',
+            chunks: ['index'],
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        publicPath: '/',
+        hot: true,
+        port: 8080,
+    },
+};
