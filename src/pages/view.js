e9b830e4d3d8730f819d9c17d4ff66d7d98a4db0
Chanya Kukulprasong <chanjeedtu75@gmail.com>
2020-06-27 17:45:19 +0900

    Implement delete button in view.html

--- src/pages/view.js
+++ src/pages/view.js
@@ -1,8 +1,15 @@
-import { getBlog, getBlogs } from '../services/Firestore';
+import { getBlog, deleteBlog } from '../services/Firestore';
 
 import './header.css';
 import './body.css';
 import './view.css';
+
+
+var sanitizeHTML = function (str) {
+    var temp = document.createElement('div');
+    temp.textContent = str;
+    return temp.innerHTML;
+};
 
 
 var url = document.location.href,
@@ -13,16 +20,28 @@ for (var i = 0, l = params.length; i < l; i++) {
     data[tmp[0]] = tmp[1];
 }
 const blogId = data.blogId;
+
+// add blog id to delete button
+var deleteButton = document.querySelector('#delete-view-blog');
+deleteButton.setAttribute("data-id", blogId);
+
 const blogData = getBlog(blogId);
 
+// add data to page-content 
 blogData.then(function (data) {
-
-    console.log(data)
     var blogTitle = document.querySelector('#blog-title');
-    blogTitle.innerHTML = data.title;
+    blogTitle.innerHTML = "<h1>" + sanitizeHTML(data.title) + "</h1>";
     var blogContent = document.querySelector('#blog-content');
     blogContent.innerHTML = data.content;
 });
 
 
+//delete button
 
+var deleteButton = document.querySelector("#delete-view-blog");
+deleteButton.addEventListener('click', async function (e) {
+    const blogId = e.target.dataset.id;
+    await deleteBlog(blogId);
+    var url = 'index.html';
+    window.location.href = url;
+});
